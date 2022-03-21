153877649c430c6e43660499527c52baaa2e1324
ldimbe20 <ldimbe20@gmail.com>
2022-03-21 13:44:25 -0500

    Created filter on title

--- sketchbookapi/views/view_artists.py
+++ sketchbookapi/views/view_artists.py
@@ -8,36 +8,36 @@ from rest_framework.decorators import action
 
 
 
+
 class ArtistView(ViewSet):
     
     def list(self, request):
         """Get a list of all artist"""
         artists = Artist.objects.all()
+        # Making query to gather all info from artists
         serializer = ArtistSerializer(artists, many=True)
+        # Serializer represents how the python data will be returned to client- We are gathering all the artists data here with many equals true
+        # Created a folder of serializers to make data clearer to read this also represents encapsulation 
         return Response(serializer.data)
     
     def retrieve(self, request, pk):
         """Get a artist"""
         artists = Artist.objects.get(pk=pk)
+        #Making query to only get artists that match the PK in the url route
         serializer = ArtistSerializer(artists)
+        # gathering only the data from that one artist
+         # Created a folder of serializers to make data clearer to read this also represents encapsulation 
         return Response(serializer.data)
     
-    # @action(methods=['get'], detail=False)
-    # #@action turns method into new route, signup, in url. 
-    # def currentUser(self, request):
-    #     """Artist request for a user to sign up for an event"""
-    #     artist = Artist.objects.get(user=request.auth.user) 
-        
-    #     # artist = Artist.objects.get(pk=pk)
-    #     # artist.artist.add(artist)
-        
-    #     serializer = ArtistSerializer(artist)
-    #     return Response(serializer.data)
     
     @action(methods=['get'], detail=False)
     def current(self, request):
-        """Only get actors back that are currently active on a book"""
+        """Only get artist back that are currently active on a book"""
+        
+        # creating a custom action that adds a new verb to URL that gets artists by currently active user
 
         artist= Artist.objects.get(user=request.auth.user)
         serializer = ArtistSerializer(artist)
         return Response(serializer.data)
+    
+    
--- sketchbookapi/views/view_comments.py
+++ sketchbookapi/views/view_comments.py
@@ -14,18 +14,25 @@ class CommentView(ViewSet):
     def list(self, request):
         """Get a list of all comments"""
         comment = Comment.objects.all()
+        # Making query to gather all info from comments
         serializer = CommentSerializer(comment, many=True)
+         # Serializer represents how the python data will be returned to client- We are gathering all the comments data here with many equals true
+        # Created a folder of serializers to make data clearer to read this also represents encapsulation 
         return Response(serializer.data)
     
     def retrieve(self, request, pk):
         """Get a comment"""
         comment = Comment.objects.get(pk=pk)
+        #retrieving data by the pk that is in the url
         serializer = CommentSerializer(comment)
+          # Serializer represents how the python data will be returned to client- We are gathering all the comments data here with many equals true
+        # Created a folder of serializers to make data clearer to read this also represents encapsulation 
         return Response(serializer.data)
     
     def destroy(self, request, pk):
         """Delete a post, current user must be associated with the post to be deleted
         """
+        # getting comment by pk and then deleting if primary key doesn't exist we get a 204 response
         try:
             post = Comment.objects.get(pk=pk)
             post.delete()
@@ -37,18 +44,20 @@ class CommentView(ViewSet):
         """Create a new comment"""
         
         user = Artist.objects.get(user=request.auth.user)
-        # post= Post.objects.get(post=request.post_id)
+        # Need to add foriegn key artists object to create new post
       
         comment = Comment.objects.create(
                 post_id=request.data['post_id'],
                 content=request.data['content'],
                 user = user,
             )
+        
+        # gathering all information to create a new comment
     
         try:
             serializer = CreateCommentSerializer(comment)
             # need to make a serializer to create json out of dictionary object
             return Response(serializer.data, status=status.HTTP_201_CREATED)
- 
+         #if some information isn't presented below response will be sent out
         except ValidationError as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
